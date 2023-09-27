ad74a71e64da3a997d3704008b1d02bf5e76cacd
rgsk <guptarahul70322@gmail.com>
2023-09-27 11:43:56 +0530

    car

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
-import { setupCounter } from "./counter.ts";
+import { Car } from "./car";
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+const canvas = document.getElementById("myCanvas") as HTMLCanvasElement | null;
+if (canvas) {
+  canvas.height = window.innerHeight;
+  canvas.width = 200;
+  const ctx = canvas.getContext("2d");
+  if (ctx) {
+    const car = new Car({ x: 100, y: 100, width: 30, height: 50 });
+    car.draw(ctx);
+  }
+}
